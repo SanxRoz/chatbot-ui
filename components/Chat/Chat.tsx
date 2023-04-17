282419c3c716fcf62a4c6df9cf57595917b9acf0
Roz <san7yx@gmail.com>
2023-04-17 17:24:12 +0000

    Brand

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -156,7 +156,7 @@ export const Chat: FC<Props> = memo(
       <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#1C1C1C]">
         {!(apiKey || serverSideApiKeyIsSet) ? (
           <div className="mx-auto flex h-full w-[300px] flex-col items-center justify-center space-y-6 sm:w-[600px]">
-            <Image src="/favicon.svg" alt="My Image" width={100} height={100} />
+            <Image className='invert dark:invert-0' src="/mndhv.svg" alt="My Image" width={100} height={100} />
             <div className="text-center text-5xl font-bold text-black dark:text-white">
               mndhv
             </div>
