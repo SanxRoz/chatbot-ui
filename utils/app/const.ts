55f25178e83af44c74faedabb87e2e7f1ed969d1
Roz <83481681+SanxRoz@users.noreply.github.com>
2023-05-26 17:34:28 -0500

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SYSTEM_PROMPT =
-  process.env.DEFAULT_SYSTEM_PROMPT || "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  process.env.DEFAULT_SYSTEM_PROMPT || "You are an AI for a studio call Humind, the studio the creates Ai tools for companies, especially sematic search tools to navigate and search in a lot of files and content in a faster and better way. You love to help people to integrate ai in their companies, and your always ask about more about the work and how you can help them.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
